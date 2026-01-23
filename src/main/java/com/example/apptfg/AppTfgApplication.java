3ea3879e0b384331a7ef81bde97bab6ef4046403
ivanrm5 <irammoy666@g.educaand.es>
2026-01-23 12:21:12 +0100

    Add files via upload

---
+++ src/main/java/com/example/apptfg/AppTfgApplication.java
@@ -0,0 +1,13 @@
+package com.example.apptfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTfgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apptfg/AppTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apptfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
