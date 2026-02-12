370be292d720644aedbee8e944c3963cf5ed4ac6
IVAN <irammoy666@g.educaand.es>
2026-02-12 11:46:35 +0100

    login completo

--- src/main/java/com/example/apptfg/controller/HomeController.java
+++ src/main/java/com/example/apptfg/controller/HomeController.java
@@ -5,9 +5,11 @@ import com.example.apptfg.model.Usuario;
 import com.example.apptfg.service.ServiceUsuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     private final ServiceUsuario serviceUsuario;
@@ -16,27 +18,14 @@ public class HomeController {
         this.serviceUsuario = serviceUsuario;
     }
 
-    @GetMapping("/home")
+    @GetMapping
     public String home() {
         return "home";
     }
 
-    @GetMapping("/home")
-    public String home(
-            @RequestParam(required = false) String username,
-            Model model
-    ) {
-        if (username == null) {
-            return "redirect:/auth/login";
-        }
-
-        Usuario usuario = serviceUsuario.buscarPorUsername(username);
-
-        if (usuario == null) {
-            return "redirect:/auth/login";
-        }
-
-        model.addAttribute("usuario", usuario);
+    @GetMapping("/usuario")
+    public String homeUsuario(@RequestParam String username, Model model) {
+        model.addAttribute("username", username);
         return "home";
     }
 }
--- src/main/java/com/example/apptfg/controller/LoginController.java
+++ src/main/java/com/example/apptfg/controller/LoginController.java
@@ -63,6 +63,5 @@ public class LoginController {
         }
         return "redirect:/auth/login";
     }
-
 }
 
